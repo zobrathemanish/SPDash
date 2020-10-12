2a25a2b8fc7173b7377866c1e787bfe65f381eec
manish <manish@bottle.com.np>
2020-10-12 16:12:13 +0545

    notifications

---
+++ app/src/main/java/com/sajiloprint/dashboard/init/CustomApplication.java
@@ -0,0 +1,71 @@
+package com.sajiloprint.dashboard.init;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.onesignal.OneSignal;
+import com.sajiloprint.dashboard.notification.MyNotificationOpenedHandler;
+import com.sajiloprint.dashboard.notification.MyNotificationReceivedHandler;
+import com.activeandroid.ActiveAndroid;
+
+/**
+ * Created by mikepenz on 27.03.15.
+ */
+public class CustomApplication extends Application {
+
+    private static Context context;
+
+    public static Context getContext() {
+        return context;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+//        Database ORMhelper class
+        ActiveAndroid.initialize(this);
+
+        context = getApplicationContext();
+        //MyNotificationOpenedHandler : This will be called when a notification is tapped on.
+        //MyNotificationReceivedHandler : This will be called when a notification is received while your app is running.
+        OneSignal.startInit(this)
+                .setNotificationOpenedHandler(new MyNotificationOpenedHandler())
+                .setNotificationReceivedHandler( new MyNotificationReceivedHandler() )
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+
+//        //initialize and create the image loader logic
+//        DrawerImageLoader.init(new AbstractDrawerImageLoader() {
+//            @Override
+//            public void set(ImageView imageView, Uri uri, Drawable placeholder, String tag) {
+//                Glide.with(imageView.getContext()).load(uri).placeholder(placeholder).into(imageView);
+//            }
+//
+//            @Override
+//            public void cancel(ImageView imageView) {
+//                //Glide.clear(imageView);
+//                Glide.with(imageView.getContext()).clear(imageView);
+//            }
+//
+//            @Override
+//            public Drawable placeholder(Context ctx, String tag) {
+//                //define different placeholders for different imageView targets
+//                //default tags are accessible via the DrawerImageLoader.Tags
+//                //custom ones can be checked via string. see the CustomUrlBasePrimaryDrawerItem LINE 111
+//                if (DrawerImageLoader.Tags.PROFILE.name().equals(tag)) {
+//                    return DrawerUIUtils.getPlaceHolder(ctx);
+//                } else if (DrawerImageLoader.Tags.ACCOUNT_HEADER.name().equals(tag)) {
+//                    return new IconicsDrawable(ctx).iconText(" ").backgroundColorRes(com.mikepenz.materialdrawer.R.color.primary).sizeDp(56);
+//                } else if ("customUrlItem".equals(tag)) {
+//                    return new IconicsDrawable(ctx).iconText(" ").backgroundColorRes(R.color.md_red_500).sizeDp(56);
+//                }
+//
+//                //we use the default one for
+//                //DrawerImageLoader.Tags.PROFILE_DRAWER_ITEM.name()
+//
+//                return super.placeholder(ctx, tag);
+//            }
+//        });
+    }
+}
